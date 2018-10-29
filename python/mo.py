e3b95b996e3e34abaf4b3521b77e9524567d163f
wangxuemei <wangxuemei@wangxuemeis-MacBook-Pro.local>
2018-10-29 11:42:11 +0800

    Add sorted similarity list for every item

--- python/mo.py
+++ python/mo.py
@@ -1,7 +1,9 @@
+# run with python3
 import numpy as np
 import re
 import math
-
+import sys
+import codecs
 
 user_list = []
 users_products = []
@@ -16,7 +18,17 @@ with open("../data/mo_itemCF_related") as infile:
             product_set.add(product)
         users_products.append(products)
 
+items_dict = dict()
+with open("../data/mo_items.output") as infile:
+    for line in infile.readlines():
+        value = line.split()
+        items_dict[value[0]] = value[1:]
+
 product_list = list(product_set)
+#print(len(product_list))
+#print(product_list)
+#print(items_dict)
+#input("press enter to continue...")
 dims = len(product_list)
 
 user_itemSet_dict = dict()
@@ -44,8 +56,19 @@ for i in range(0, dims):
             A[i][j] = similarity[i][j]
 
 similarity_normed = A * 1./np.max(A, axis = 0)
+
+normed_sorted = np.argsort(-similarity_normed, axis = 1)
+without_normed_sorted = np.argsort(-A, axis = 1)
+for i in range(0, dims):
+    for j in range(1, 11):
+        #print(items_dict[product_list[i]], items_dict[product_list[j]], similarity_normed[i][j]) 
+        #print(items_dict[product_list[i]],
+                #items_dict[product_list[normed_sorted[i][j]]],
+                #similarity_normed[i][normed_sorted[i][j]]) 
+        print(items_dict[product_list[i]],
+                items_dict[product_list[without_normed_sorted[i][j]]],
+                A[i][without_normed_sorted[i][j]]) 
+        #input("press key to continue...")
+
 #print np.set_printoptions(threshold=np.inf)
-#print similarity_normed
-
-
 #save  procuct_list,  user_itemSet_dict, item_matrix
